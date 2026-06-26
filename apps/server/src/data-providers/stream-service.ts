302d8974d1015636b5aa851508f30c141cb57e78
eranelbaz <eranelbaz97+github@gmail.com>
2026-06-26 18:30:38 +0300

    chore: remove dead stream-service.ts

--- apps/server/src/data-providers/stream-service.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-import { Candle } from '../candles/candle.interface';
-
-export const STREAM_SERVICE = 'STREAM_SERVICE';
-
-export type StreamService = {
-  minuteBars(symbol: string, _timeframe?: string): Observable<Candle>;
-};
